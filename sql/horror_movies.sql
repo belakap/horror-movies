ecaa9a43b2fde703f8e28918e637dcdadf336e4b
ngomba <belakap24@gmail.com>
2024-10-10 19:39:30 -0400

    sql/horror_movies.sql

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,12 @@
--- Add your SQL here
+SELECT 
+  id AS Movie_ID, 
+  name AS Movie_Title, 
+  imdb_rating AS Rating
+FROM 
+  movies
+WHERE 
+  genre = 'horror' 
+  AND year <= 1985
+ORDER BY 
+  imdb_rating DESC
+LIMIT 3;
